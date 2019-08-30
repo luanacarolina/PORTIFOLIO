06702d9bc8450931006a6dcccc8da12d578d18bf
luana <luanalcj1@hotmail.com>
2019-08-30 20:43:34 -0300

    alterações pasta

---
+++ js/init.js
@@ -0,0 +1,28 @@
+(function ($) {
+    $(function () {
+        $('.sidenav').sidenav();
+        $('.parallax').parallax();
+        $('.dropdown-trigger').dropdown();
+        $('.slider').slider({
+            duration: 500,
+            interval: 2000,
+            indicators: false
+        });
+
+    });
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
